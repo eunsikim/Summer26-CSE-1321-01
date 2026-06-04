5c8a462bc618624d53658a48fad38daea165735e
eunsikim <kimeunsik229@gmail.com>
2026-06-04 15:31:55 -0400

    in-class 06/04 part I

---
+++ Week 1/comments_indentations.py
@@ -0,0 +1,7 @@
+print('Hello World') #This is a comment
+"""
+    This is
+    a multi
+    line
+    comment
+"""
---
+++ Week 1/types_1.py
@@ -0,0 +1,2 @@
+print("5") # String type cannot be used for math. op.
+print(5) # Numeric type can be used for math. op.
