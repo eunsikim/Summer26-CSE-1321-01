ca4e263ba5c747fb65031cd959d13550dc253161
eunsikim <kimeunsik229@gmail.com>
2026-06-16 14:43:26 -0400

    in class FOR loop and ex 1

---
+++ Week 3/for_1.py
@@ -0,0 +1,8 @@
+def main():
+	my_string = "Hello World"
+    
+	for character in my_string:
+		print(character, end="")
+
+if __name__ == "__main__":
+    main()
---
+++ Week 3/in_class_ex_1.py
@@ -0,0 +1,14 @@
+def main():
+    my_string = "HelloWorld"
+
+    counter = 0
+
+    for character in my_string:
+        if character.isupper() and counter != 0:
+            print(" ", end="")
+
+        print(character, end="")
+        counter = 1
+
+if __name__ == "__main__":
+    main()
